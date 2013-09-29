b5e5d26ca3861168d7594b55bbbad1ff40a6b33c
Wiliam Souza <wiliamsouza83@gmail.com>
2013-09-29 13:19:14 -0300

    Added include, lib, adb.py removed include, lib from gitignore

---
+++ adblib/bin/adb.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+import sys
+
+from ctypes import cdll
+from ctypes import c_char_p
+
+sys.argv.pop(0)
+argc = len(sys.argv)
+argv = (c_char_p * argc)()
+for n in range(argc):
+    argv[n] = sys.argv[n]
+
+adblib = cdll.LoadLibrary('adblib.so')
+adblib.adb_trace_init()
+
+adblib.adb_commandline(argc, argv)
