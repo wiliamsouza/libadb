078bda751cbb9f28af3beea5a2bfdd16a1c2cbeb
Wiliam Souza <wiliamsouza83@gmail.com>
2013-09-29 13:16:43 -0300

    Added initial python lib file

---
+++ adblib/adblib.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+import sys
+
+from ctypes import cdll
+from ctypes import c_char_p
+from ctypes.util import find_library
+
+
+logger = logging.getLogger('adblib.adblib')
+
+
+lib_names = ['adblib']
+
+if lib_names:
+    for lib_name in lib_names:
+        lib_path = find_library(lib_name)
+        if not lib_path is None:
+            break
+try:
+    lib_path = os.environ['ADB_LIBRARY_PATH']
+except KeyError:
+    lib_path = None
+
+if lib_path is None:
+    raise Exception('Could not find the ADB library (tried "%s"). '
+                    'Try setting ADB_LIBRARY_PATH variable.' %
+                    '", "'.join(lib_names))
+
+sys.argv.pop(0)
+argc = len(sys.argv)
+argv = (c_char_p * argc)()
+for n in range(argc):
+    argv[n] = sys.argv[n]
+
+adblib = cdll.LoadLibrary(lib_path)
+adblib.adb_trace_init()
+
+adblib.adb_commandline(argc, argv)
+
+connect = 'host:connect:192.168.1.2:5555'
+fd = adblib.adb_query(connect)
+print fd
+
+# adb.h
+print(adblib.local_init)
+print(adblib.local_connect)
+print(adblib.local_connect_arbitrary_ports)
+
+print(adblib.usb_init)
